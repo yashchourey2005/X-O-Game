b423fa185ec54bccbcda3c4f9f9aa720eeb94cc5
yashchourey <yashchourey2005@gmail.com>
2025-07-13 01:16:46 +0530

    Some adv. features

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ let h1 = document.querySelector("h1");
 let winner = document.querySelector(".winner");
 let body = document.querySelector("body");
 
+let container = document.querySelector(".container");
+
 
 let turnO = true;
+let finish = false;
 
 let winningPattern = [[0,1,2],[0,4,8],[0,3,6],[1,4,7],[2,5,8],[2,4,6],[3,4,5],[6,7,8]];
 
@@ -23,7 +26,10 @@ for(let box of boxes){
             box.classList.remove("active");
         }
         showWinner();
-        checkDrow();
+
+        if(!finish){
+            checkDrow();
+        }
     })
 }
 
@@ -44,6 +50,7 @@ reset.addEventListener("click", ()=>{
     turnO = true;
     activeBoxes();
     winner.classList.add("hide");
+    finish = false;
 })
 
 function checkDrow(){
@@ -56,6 +63,8 @@ function checkDrow(){
     if(cnt == 9){
         winner.innerText = "Oops!....Game is Drow";
         winner.classList.remove("hide");
+        drowBackground();
+        finish = true;
     }
 }
 
@@ -72,19 +81,54 @@ function showWinner(){
         if(val1 == val2 && val2 == val3){
             console.log("Winner is : ", val1);
             winner.innerHTML = `WINNER is <b>${val1}</b>`
-            winnerBackground();
             winner.classList.remove("hide");
+            winnerBackground();
             disableBoxes();
+            finish = true;
         }
     }
     }
+}
 
     function winnerBackground(){
         body.classList.add("green");
+        winner.style.color = "black";
         setTimeout(()=>{
             body.classList.remove("green");
         }, 2000);
     }
 
+    function drowBackground(){
+        body.classList.add("red");
+        setTimeout(async ()=>{
+            body.classList.remove("red");
+            await dogImg();
+        }, 2000);
+    }
+
+
+
+
+    //-------------------------------------------------------------------------------------
+    // This is informal code 
+
+    let url = "https://dog.ceo/api/breeds/image/random"
     
-}
+
+    async function dogImg(){
+        let img = document.createElement("img");
+        let res = await axios.get(url);
+
+        container.classList.add("hide");
+        img.setAttribute("src", res.data.message);
+        body.appendChild(img);
+        img.style.height = "50%";
+        img.style.width = "50%";
+
+        setTimeout(()=>{
+            body.removeChild(img);
+            container.classList.remove("hide");
+        },4000);
+        
+    }
+
